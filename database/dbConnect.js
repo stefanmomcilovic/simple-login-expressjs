eebaec4e0132022957b21faafc406a9badf699c0
Stefan Momcilovic <52540681+stefanmomcilovic@users.noreply.github.com>
2022-03-04 14:49:22 +0100

    Update dbConnect.js

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('nodejs_test', 'root', '', {
+const sequelize = new Sequelize('lukama5_nodejs_simplelogin_test', 'lukama5_simpleloginuser', 'secure253.inmotionhosting.com', {
     host: 'localhost',
     dialect: 'mysql'
 });
